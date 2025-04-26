3724c634a5ca4affa7ca0ad52213c162bfa6f667
stevebendict <stevebendict@gmail.com>
2025-04-26 20:21:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ setInterval(changeColor, 10000);
 
 const typewriter = new Typed('.js-typer', {
     strings: [
-        'We provides elite business school assignment help.',
+        'We provides business school assignment help.',
         '100% original, Turnitin-verified work.',
         'Secure crypto payments.'
     ],
